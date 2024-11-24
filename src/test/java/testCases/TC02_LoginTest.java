242b3d6dfc1e052bea4be1e500d6ab1548602884
Chiru <br.chiru@gmail.com>
2024-11-24 16:34:25 +0000

    TC02_LoginTest class has been updated after clearing the warnings

--- src/test/java/testCases/TC02_LoginTest.java
+++ src/test/java/testCases/TC02_LoginTest.java
@@ -4,8 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import pageObjects.HomePage;
-import pageObjects.LoginPage;
-import pageObjects.MyAccountPage;
 import testBase.BaseClass;
 
 public class TC02_LoginTest extends BaseClass
